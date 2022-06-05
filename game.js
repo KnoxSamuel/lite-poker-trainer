7f2beb03af13ff6e5b16a597f959c9c87bbbd638
KnoxSamuel <knoxsa@oregonstate.edu>
2022-06-04 22:21:24 -0700

    dependency fix

--- game.js
+++ game.js
@@ -98,36 +98,14 @@ Game.prototype.renderWinningAnimation = function() {
 
 
 Game.prototype.compareEquities = function(chosenHand, loserHand) {
-    const Table = new TexasHoldem();
-    Table
-    .addPlayer(["Qs", "Ks"])
-    .addPlayer(["Qd", "Kd"])
-    
-    .setBoard(["Js","Ts","5h","Td"])
-    // or
-    .boardAction(board => {
-        board
-        .setFlop(["Js", "Ts", "5h"])
-        .setTurn("Td")
-    })
-    ;
-    
-    const Result = Table.calculate();
-    
-    Result.getPlayers().forEach(player => {
-    console.log(`${player.getName()} - ${player.getHand()} - Wins: ${player.getWinsPercentageString()} - Ties: ${player.getTiesPercentageString()}`);
-    });
-    
-    console.log(`Board: ${Result.getBoard()}`);
-    console.log(`Iterations: ${Result.getIterations()}`);
-    console.log(`Time takes: ${Result.getTime()}ms`);
-    
-    // Outputs:
-    // Player #1 - QsKs - Wins: 20.45% - Ties: 79.55%
-    // Player #2 - QdKd - Wins: 0.00% - Ties: 79.55%
-    // Board: JsTs5hTd
-    // Iterations: 44
-    // Time takes: 8ms
+    const player1Cards = CardGroup.fromString('JhJs');
+    const player2Cards = CardGroup.fromString('JdQd');
+    const board = CardGroup.fromString('7d9dTs');
+
+    const result = OddsCalculator.calculate([player1Cards, player2Cards], board);
+
+    console.log(`Player #1 - ${player1Cards} - ${result.equities[0].getEquity()}%`);
+    console.log(`Player #2 - ${player2Cards} - ${result.equities[1].getEquity()}%`);
 }
 
 
